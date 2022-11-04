d7f661a1b6d4eaaac22f10df20c11d8421d00f18
AnVinh2811 <0306201402@caothang.edu.vn>
2022-11-05 01:10:56 +0700

    để lộn thư mục sr anh em

--- lib/views/searchSreen.dart
+++ lib/views/searchSreen.dart
@@ -1,1 +1,27 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class searchSreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Row(
+          children: [
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Tìm kiếm',
+
+                  //prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.clear))
+          ],
+        ),
+      ),
+    );
+  }
+}
